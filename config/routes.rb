72529819317c89679eef584c4d03654a1ce3113b
Samuel Masters <redeyetuning@gmail.com>
2018-06-11 13:01:37 +0400

    Add Sessions controller with #new and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+get '/signup', to: 'users#new'
+post '/signup', to: 'users#create'
+
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+
 resources :users
+
 
 end
