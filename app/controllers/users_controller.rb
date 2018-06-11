e2e88a86c7433959fddc3b603898464354eefd28
Samuel Masters <redeyetuning@gmail.com>
2018-06-11 11:21:12 +0400

    Add new user route, view, and method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,22 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:success] = "User account created succesfully"
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+
+	end
+
 
 	private
 
 		def user_params
-			params.require(:user).permit(:name,:email,:password, :password_confirmation)
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end	
 end
