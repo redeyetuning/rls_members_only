ac4ce3bd8479cdfa0742b91b3c9a2bc7250aed1f
Samuel Masters <redeyetuning@gmail.com>
2018-06-11 13:08:57 +0400

    Add remember_token to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
+	before_create :create_remember_token
+
 	validates :name, presence: true, length: {minimum:6, maximum:50} 
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,4 +13,10 @@ class User < ApplicationRecord
 
 	has_secure_password	
 	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+
+
+		def create_remember_token
+			self.remember_token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_094218) do
+ActiveRecord::Schema.define(version: 2018_06_11_090609) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_06_10_094218) do
     t.boolean "member", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
