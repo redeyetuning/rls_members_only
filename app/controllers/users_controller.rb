a5ac5749784272b711ed9bfd52337b8295162a8f
Samuel Masters <redeyetuning@gmail.com>
2018-06-11 09:25:28 +0400

    Add Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+
+	private
+
+		def user_params
+			params.require(:user).permit(:name,:email,:password, :password_confirmation)
+		end	
+end
