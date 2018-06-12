5328719e6f94d3c793d3e2eb7314409d356083bb
Samuel Masters <redeyetuning@gmail.com>
2018-06-12 10:00:17 +0400

    Add Session_helper #current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,14 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			flash[:sucess] = "Welcome #{user.name}"
 			sign_in(user)
+			self.current_user = user
 			redirect_to user_path(user)
 			
 		else
 			flash[:error] = "Username or password incorrect"
 			render 'new'
-
 		end
 	end
+
+	
 end
