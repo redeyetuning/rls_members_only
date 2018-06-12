f10c5b986353e92050392ca2b4705d57127912ba
Samuel Masters <redeyetuning@gmail.com>
2018-06-12 21:45:43 +0400

    Add basic static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+	root 'static_pages#home'
 
-get '/signup', to: 'users#new'
-post '/signup', to: 'users#create'
+  get '/', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
+	
+	get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'
 
-get '/login', to: 'sessions#new'
-post '/login', to: 'sessions#create'
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
 
-delete '/logout', to: 'sessions#destroy'
+	delete '/logout', to: 'sessions#destroy'
 
-resources :users
+	resources :users
 
 
 end
