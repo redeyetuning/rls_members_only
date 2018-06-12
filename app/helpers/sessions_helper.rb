e4688e3520ce4154c3a8089872128cbcd12cae2c
Samuel Masters <redeyetuning@gmail.com>
2018-06-12 09:13:29 +0400

    Add login session_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+	def sign_in(user)
+		remember_token = User.new_token
+		cookies.permanent[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+	end
+
 end
