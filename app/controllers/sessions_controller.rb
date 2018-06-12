996eb7d17b3080223d4ec6b00e7ec4b30384cbad
Samuel Masters <redeyetuning@gmail.com>
2018-06-12 09:30:35 +0400

    Add Sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(email: params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			flash[:sucess] = "Welcome #{user.name}"
+			sign_in(user)
+			redirect_to user_path(user)
+			
+		else
+			flash[:error] = "Username or password incorrect"
+			render 'new'
+
+		end
 	end
 end
